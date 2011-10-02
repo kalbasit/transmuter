a2612c09763843f10ba04fcb2e783a3da8e7d9d0
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-02 14:30:02 +0200

    spec_helper.rb: ROOT_PATH was not defined.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
-$:.push File.expand_path("../lib", __FILE__)
+ROOT_PATH = File.expand_path('..', __FILE__)
+$:.push File.expand_path("#{ROOT_PATH}/lib")
 
 require 'rubygems'
 require 'rspec'
