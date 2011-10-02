01189e95d52d16d3c033da964b0ee8dffa235b18
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-02 18:21:36 +0200

    cli: Add an input argument.

--- lib/transmuter/cli/thor.rb
+++ lib/transmuter/cli/thor.rb
@@ -4,5 +4,12 @@ require 'thor/group'
 module Transmuter
   class CLI < Thor::Group
     desc "Transmute one file format into another"
+
+    argument :input,
+      type: :string,
+      required: true,
+      aliases: "-i",
+      desc: "The input file name."
+
   end
 end
--- spec/transmuter/cli/thor_spec.rb
+++ spec/transmuter/cli/thor_spec.rb
@@ -10,7 +10,18 @@ describe CLI do
       it { should respond_to(:class_option) }
       it { should respond_to(:argument) }
       its (:desc) { should_not be_empty }
+      its (:arguments) { should_not be_empty }
     end
+
+    describe "input" do
+      it "should have an arguments :input defined" do
+        CLI.arguments.any? { |arg| arg.name == 'input' }.should be_true
+      end
+
+      it "should be required" do
+        -> { CLI.new }.should raise_error Thor::RequiredArgumentMissingError,
+          "No value provided for required arguments 'input'"
+      end
     end
   end
 end
