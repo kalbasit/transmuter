361187396a593e9e7367beb5f8c1945051178c18
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-10-03 12:28:45 +0200

    bin/transmute: Make sure pygments and wkhtmltopdf exists.

--- lib/transmuter.rb
+++ lib/transmuter.rb
@@ -1,5 +1,21 @@
 ROOT_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
+# Taken from hub
+# https://github.com/defunkt/hub/blob/master/lib/hub/context.rb#L186
+# Cross-platform way of finding an executable in the $PATH.
+#
+# which('ruby') #=> /usr/bin/ruby
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = "#{path}/#{cmd}#{ext}"
+      return exe if File.executable? exe
+    }
+  end
+  return nil
+end
+
 require "transmuter/version"
 require "transmuter/core_ext"
 require "transmuter/format"
